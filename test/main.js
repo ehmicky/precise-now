9d8cad2460da449c1a4bff158f21015f371af6c1
ehmicky <ehmicky@gmail.com>
2019-08-25 17:13:45 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -1,6 +1,10 @@
+import { promisify } from 'util'
+
 import test from 'ava'
 
 import now from '../src/main.js'
+
+const pSetTimeout = promisify(setTimeout)
 
 test('Returns an integer', t => {
   const time = now()
@@ -15,3 +19,21 @@ test('Returns the time since the library was loaded', t => {
 })
 
 const LOADED_TIME_THRESHOLD = 6e10
+
+test('Returns the time in nanoseconds', async t => {
+  const start = now()
+
+  await pSetTimeout(TIMEOUT_MILLESECS)
+
+  const duration = now() - start
+
+  t.true(duration > TIMEOUT_MIN)
+  t.true(duration < TIMEOUT_MAX)
+})
+
+const MILLISECS_TO_NANOSECS = 1e6
+const TIMEOUT_MILLESECS = 1e3
+const TIMEOUT_NANOSECS = TIMEOUT_MILLESECS * MILLISECS_TO_NANOSECS
+const TOLERANCE = 2
+const TIMEOUT_MIN = TIMEOUT_NANOSECS / TOLERANCE
+const TIMEOUT_MAX = TIMEOUT_NANOSECS * TOLERANCE
