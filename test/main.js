acdbcd89b1a8ff362b828d515db054a6f22094cf
ehmicky <ehmicky@gmail.com>
2019-08-25 17:13:25 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -7,3 +7,11 @@ test('Returns an integer', t => {
 
   t.true(Number.isInteger(time))
 })
+
+test('Returns the time since the library was loaded', t => {
+  const time = now()
+
+  t.true(time < LOADED_TIME_THRESHOLD)
+})
+
+const LOADED_TIME_THRESHOLD = 6e10
