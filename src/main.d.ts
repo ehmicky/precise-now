29be7e817882f67f5df1da7d69c87efb428bf299
ehmicky <ehmicky@gmail.com>
2022-12-11 19:19:52 +0100

    Add example

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,4 +1,11 @@
 /**
  * Return the number of nanoseconds since the time origin.
+ *
+ * @example
+ * ```js
+ * const start = preciseNow()
+ * const end = preciseNow()
+ * const duration = end - start
+ * ```
  */
 export default function preciseNow(): number
