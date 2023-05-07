8b063a6ce2ca05c381c45e3991ddf46f593c505b
ehmicky <ehmicky@gmail.com>
2023-05-07 19:57:37 +0100

    Simplify benchmarks

--- benchmark/tasks.js
+++ benchmark/tasks.js
@@ -1,7 +1,3 @@
-// TODO: `performance` is a global variable since Node 16.0.0, i.e. remove this
-// import after dropping support for Node <16.0.0
-// eslint-disable-next-line no-shadow
-import { performance } from 'node:perf_hooks'
 import process from 'node:process'
 
 export const hrtime = () => {
