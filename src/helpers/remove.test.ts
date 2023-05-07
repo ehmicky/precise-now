ac82e49c50892e7c18ed1de8acc1902aaf7c1e19
ehmicky <ehmicky@gmail.com>
2023-05-07 18:13:46 +0100

    Fix tests on Node 16

--- src/helpers/remove.test.ts
+++ src/helpers/remove.test.ts
@@ -1,7 +1,3 @@
-// TODO: remove after dropping support for Node <16.0.0
-// eslint-disable-next-line @typescript-eslint/no-shadow
-import { performance } from 'node:perf_hooks'
-
 // Mimics browsers by removing Node.js specific API
 export const removeHrtime = () => {
   // eslint-disable-next-line fp/no-mutation, n/prefer-global/process
@@ -10,9 +6,6 @@ export const removeHrtime = () => {
 
 // Mimics platforms without `performance.now()` by removing it
 export const removePerformanceNow = () => {
-  // eslint-disable-next-line fp/no-mutation
-  performance.now = undefined as never
-
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   if (globalThis?.performance?.now !== undefined) {
     // eslint-disable-next-line fp/no-mutating-methods
